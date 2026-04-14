aa0fbf0094efc3c1251c5f49c1ec77352571b3aa
Syed Bahauddin <bahauddin1097@gmail.com>
2026-04-14 14:49:51 +0530

    create_superuser.py file for Render admin

---
+++ Backend/create_superuser.py
@@ -0,0 +1,18 @@
+import django, os
+
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clickmart_main.settings')
+django.setup()
+
+User = get_user_model()
+
+username = os.environ.get('SUPERUSER_USERNAME', 'mngr_sb97')
+email = os.environ.get('SUPERUSER_EMAIL', 'bahauddin@gmail.com')
+password = os.environ.get('SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser '{username}' created successfully.")
+else:
+    print(f"Superuser '{username}' already exists. Skipping.")
